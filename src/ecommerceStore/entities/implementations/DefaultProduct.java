4694b2d05754beaac79a2d5c686f4e908063811d
Victor Pena <victor.javier.pena210@gmail.com>
2025-05-30 08:12:29 -0500

    DefaultProductManagementService test

--- src/ecommerceStore/entities/implementations/DefaultProduct.java
+++ src/ecommerceStore/entities/implementations/DefaultProduct.java
@@ -3,17 +3,38 @@ package ecommerceStore.entities.implementations;
 import ecommerceStore.entities.Product;
 
 public class DefaultProduct implements Product {
+	
+	private int id;
+	private String productName;
+	private String productCategory;
+	private double price;
+	
+	public DefaultProduct() {
+		
+	}
+	
+
+	public DefaultProduct(int id, String productName, String productCategory, double price) {
+		this.id = id;
+		this.productName = productName;
+		this.productCategory = productCategory;
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "DefaultProduct [id=" + id + ", productName=" + productName + ", productCategory=" + productCategory
+				+ ", price=" + price + "]";
+	}
 
 	@Override
 	public int getId() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.id;
 	}
 
 	@Override
 	public String getProductName() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.productName;
 	}
 
 }
